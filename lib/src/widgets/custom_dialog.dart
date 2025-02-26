bbbb19df5d122ae10582038d73cd75a66401b243
Anthony Aniobi <anthonyaniobi198@gmail.com>
2025-02-26 05:26:51 +0100

    update compile sdk version

--- lib/src/widgets/custom_dialog.dart
+++ lib/src/widgets/custom_dialog.dart
@@ -33,7 +33,7 @@ class CustomAlertDialog extends StatelessWidget {
       children.add(new Padding(
         padding: titlePadding!,
         child: new DefaultTextStyle(
-          style: Theme.of(context).textTheme.titleSmall,
+          style: Theme.of(context).textTheme.titleLarge,
           child: new Semantics(child: title, namesRoute: true),
         ),
       ));
@@ -43,7 +43,7 @@ class CustomAlertDialog extends StatelessWidget {
       child: new Padding(
         padding: contentPadding,
         child: new DefaultTextStyle(
-          style: context.textTheme().subtitle1!,
+          style: Theme.of(context).textTheme.titleMedium!,
           child: content,
         ),
       ),
